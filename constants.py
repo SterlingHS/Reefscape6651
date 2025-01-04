24f07b9b9558f6ea57e93ed49a21025796a07b20
Alexis Rivera <arivera@sps5.org>
2025-01-03 20:21:02 -0600

    Fixed Constants

--- constants.py
+++ constants.py
@@ -26,8 +26,8 @@ class DriveConstants:
     kFrontLeftAbsoluteEncoderPort = 13
     kFrontLeftAbsoluteEncoderOffsetRad = -1.270136
     kFrontLeftAbsoluteEncoderReversed = False
-    kFrontLeftForwardPIDk = [0.62076, 0, 0, 0.17689, 2.6784, 0.063868] # [P, I, D, kS, KV, kA] - From reca.lc/drive kV = 2.3, kA = 0.06 - Use SysID for kS
-    #kFrontLeftForwardPIDk = [0.62076, 0, 0, 1.7689, 26.784, 0.63868] # [P, I, D, kS, KV, kA] - From reca.lc/drive kV = 2.3, kA = 0.06 - Use SysID for kS
+    #kFrontLeftForwardPIDk = [0.62076, 0, 0, 0.17689, 2.6784, 0.063868] # [P, I, D, kS, KV, kA] - From reca.lc/drive kV = 2.3, kA = 0.06 - Use SysID for kS
+    kFrontLeftForwardPIDk = [0.62076, 0, 0, 1.7689, 26.784, 0.63868] # [P, I, D, kS, KV, kA] - From reca.lc/drive kV = 2.3, kA = 0.06 - Use SysID for kS
 
     kFrontRightDriveMotorPort = 22
     kFrontRightTurningMotorPort = 21
@@ -36,8 +36,8 @@ class DriveConstants:
     kFrontRightAbsoluteEncoderPort = 23
     kFrontRightAbsoluteEncoderOffsetRad = 1.67050507
     kFrontRightAbsoluteEncoderReversed = False
-    kFrontRightForwardPIDk = [0.62076, 0, 0, 0.25005, 2.6287, 0.063868] # [P, I, D, kS, KV, kA] - From reca.lc/drive kV = 2.3, kA = 0.06 - Use SysID for kS
-    #kFrontRightForwardPIDk = [0.62076, 0, 0, 2.5005, 26.287, 0.63868] # [P, I, D, kS, KV, kA] - From reca.lc/drive kV = 2.3, kA = 0.06 - Use SysID for kS
+    #kFrontRightForwardPIDk = [0.62076, 0, 0, 0.25005, 2.6287, 0.063868] # [P, I, D, kS, KV, kA] - From reca.lc/drive kV = 2.3, kA = 0.06 - Use SysID for kS
+    kFrontRightForwardPIDk = [0.62076, 0, 0, 2.5005, 26.287, 0.63868] # [P, I, D, kS, KV, kA] - From reca.lc/drive kV = 2.3, kA = 0.06 - Use SysID for kS
 
     kBackLeftDriveMotorPort = 32
     kBackLeftTurningMotorPort = 31
@@ -46,8 +46,8 @@ class DriveConstants:
     kBackLeftAbsoluteEncoderPort = 33
     kBackLeftAbsoluteEncoderOffsetRad = -0.3021942
     kBackLeftAbsoluteEncoderReversed = False
-    kBackLeftForwardPIDk = [0.62076, 0, 0, 0.1529, 2.704, 0.063868] # [P, I, D, kS, KV, kA] - From reca.lc/drive kV = 2.3, kA = 0.06 - Use SysID for kS
-    #kBackLeftForwardPIDk = [0.62076, 0, 0, 1.529, 27.04, 0.63868] # [P, I, D, kS, KV, kA] - From reca.lc/drive kV = 2.3, kA = 0.06 - Use SysID for kS
+    #kBackLeftForwardPIDk = [0.62076, 0, 0, 0.1529, 2.704, 0.063868] # [P, I, D, kS, KV, kA] - From reca.lc/drive kV = 2.3, kA = 0.06 - Use SysID for kS
+    kBackLeftForwardPIDk = [0.62076, 0, 0, 1.529, 27.04, 0.63868] # [P, I, D, kS, KV, kA] - From reca.lc/drive kV = 2.3, kA = 0.06 - Use SysID for kS
 
     kBackRightDriveMotorPort = 42
     kBackRightTurningMotorPort = 41
@@ -56,11 +56,11 @@ class DriveConstants:
     kBackRightAbsoluteEncoderPort = 43
     kBackRightAbsoluteEncoderOffsetRad = 0.409157827
     kBackRightAbsoluteEncoderReversed = False
-    kBackRightForwardPIDk = [0.62076, 0, 0, 0.20876, 2.6634, 0.063868] # [P, I, D, kS, KV, kA] - From reca.lc/drive kV = 2.3, kA = 0.06 - Use SysID for kS
-    #kBackRightForwardPIDk = [0.62076, 0, 0, 2.0876, 26.634, 0.63868] # [P, I, D, kS, KV, kA] - From reca.lc/drive kV = 2.3, kA = 0.06 - Use SysID for kS
+    #kBackRightForwardPIDk = [0.62076, 0, 0, 0.20876, 2.6634, 0.063868] # [P, I, D, kS, KV, kA] - From reca.lc/drive kV = 2.3, kA = 0.06 - Use SysID for kS
+    kBackRightForwardPIDk = [0.62076, 0, 0, 2.0876, 26.634, 0.63868] # [P, I, D, kS, KV, kA] - From reca.lc/drive kV = 2.3, kA = 0.06 - Use SysID for kS
 
     #THIS IS IN METERS PER SECOND. This means at 100% speed how fast is the robot going. I suggest we run tests to figure this out. We can use the navx to display the speed in meters per second and give the robot max power without the limiters.
-    kPhysicalMaxSpeedMetersPerSecond = 9 #9 MPS is about 20 miles per hour 
+    kPhysicalMaxSpeedMetersPerSecond = 5 #5 MPS is about 11 miles per hour 
 
     kMaxTurnRateDegPerS = 300 
     kMaxTurnAccelerationDegPerSSquared = 100 
@@ -78,8 +78,8 @@ class DriveConstants:
         Translation2d(-kWheelBase/2, -kTrackWidth/2) #BR
     )
     #this ends up being the damp factor. Right now this is 4.5/9 meaning the max output of the motors should be 50%
-    kTeleDriveMaxSpeedMetersPerSecond = 6.8 #14.5*0.3048 #14.5 feet per second into meters
-    kTeleDriveMaxAngularRadiansPerSecond = 12 #(14.5*12)*0.3048/2 #Transformed 14.5 feet into radians per sec
+    kTeleDriveMaxSpeedMetersPerSecond = 5 #17*0.3048 #17 feet per second into meters
+    kTeleDriveMaxAngularRadiansPerSecond = 8.5 # 17 ft/sec * (2pi radians / (2pi* 2 ft)) #Transformed 17 feet/sec into radians/sec
 
     kTeleDriveMaxAccelerationUnitsPerSeconds = kTeleDriveMaxSpeedMetersPerSecond #Taken from MaxSpeedDrive
     kTeleDriveMaxAngularAccelerationUnitsPerSeconds = 0.8 #2*12*0.3048/2 #Transformed from MaxAcceleration
@@ -113,7 +113,7 @@ class OIConstants:
 
 class AutoConstants:
     from constants import DriveConstants
-    kMaxSpeedMetersPerSecond = 6
+    kMaxSpeedMetersPerSecond = 5
     kMaxAccelerationMetersPerSecondSquared = 3
     kPThetaController = 1
     kThetaControllerConstraints = wpimath.trajectory.TrapezoidProfileRadians.Constraints(DriveConstants.kMaxTurnRateRadPerS, DriveConstants.kMaxTurnAccelerationRadPerSSquared)
